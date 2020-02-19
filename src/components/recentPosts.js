d79f083236e3f7c5208ba825eb490cffc6d0dd06
SethaReno <51383197+SethaReno@users.noreply.github.com>
2020-02-18 20:05:27 -0700

    created a post component

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,53 +1,47 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 
 import * as actions from '../actions';
 
+import Post from './post';
+
 class RecentPosts extends Component {
 
     componentDidMount() {
-      this.props.fetchRecentPosts();
+        this.props.fetchRecentPosts();
     }
 
     renderPosts = function() {
-      const posts = this.props.recentPosts.map((post, index) => {
-        
-        if (index < 3) {
-           return (
-          <li key={index}>
-              {post.title}
-          </li>
-          )
-        }
-      })
+        const posts = this.props.recentPosts.map((post, index) => {
+            if(index < 3) {
+                return (
+                    <Post {...post} key={index}/>
+                )   
+            }
+        })
         return posts
     }
 
-  render() {
-    return (
-      <div className="recent-posts">
-        <div className="recent_posts_wrapper">
-          <div className="recent-posts_heading">Recent Posts</div>
-          <ul className="recent-posts_posts">
-           {this.renderPosts()}
-          </ul>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="recent-posts">
+                <div className="recent-posts__wrapper">
+                    <div className="recent-posts__heading">Recent Posts</div>
+                    <ul className="recent-posts__posts">
+                        {this.renderPosts()}
+                    </ul>
+                </div>
+
+            </div>
+        )
+    }
 }
 
 function mapStateToProps(state) {
-  return {
-    recentPosts: state.posts.recentPosts
-  }
-}
-
-function mapStateToProps(state) {
-  return {
-    recentPosts: state.posts.recentPosts
-  }
+    return {
+        recentPosts: state.posts.recentPosts
+    }
 }
 
 export default connect(mapStateToProps, actions)(RecentPosts);
